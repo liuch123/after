8585008c7a0c4da86d46e7b85f90d0f88f377704
jjzyy123 <2485206211@qq.com>
2023-06-15 21:34:30 +0800

    jzy员工管理

---
+++ src/main/java/edu/czjt/reggie/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package edu.czjt.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.Employee;
+
+/**
+ * Created by jzy on 2023/3/16
+ */
+public interface EmployeeService extends IService<Employee> {
+}
